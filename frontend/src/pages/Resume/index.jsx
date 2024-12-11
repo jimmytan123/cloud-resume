3a81787ebed046365feb9c9747484d44fdcb0137
Jimmy Tan <jimmytan0424@gmail.com>
2024-12-11 14:12:54 -0800

    Test

--- frontend/src/pages/Resume/index.jsx
+++ frontend/src/pages/Resume/index.jsx
@@ -28,6 +28,7 @@ const Resume = () => {
   return (
     <Wrapper>
       <header className="heading">
+        <p>This is a test</p>
         <h1>Jimmy(Zhixi) Tan</h1>
         {isPending && <p className="loading-message small-text">Loading</p>}
         {error && (
